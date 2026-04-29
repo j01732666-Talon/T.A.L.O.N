cfc615710bfbddf712faae1407bf4ea511eeaf80
Jose Miguel Munoz <JMMunoz@refisal.com.co>
2026-04-29 09:47:29 -0500

    chore: eliminación de scripts utilitarios y de prueba obsoletos

--- src/utils/consultar_db.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-# El parámetro read_only=True permite entrar aunque Streamlit esté corriendo
-con = duckdb.connect("datalake_local/talon_metastore.duckdb", read_only=True)
-
-print("--- USUARIOS REGISTRADOS ---")
-print(con.sql("SELECT * FROM usuarios_sistema").df())
-
-con.close()
